a3d966f624a3f8dfffc4377acfc2ab955278590c
Archie <archiealvarado20@gmail.com>
2026-03-26 20:29:46 +0800

    update Again2

--- app/ui/auth/Register.tsx
+++ app/ui/auth/Register.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-import "@/app/ui/auth/login.css";
+import "@/app/ui/auth/Login.css";
 
 export default function Register() {
   const [showPassword, setShowPassword] = useState(false);
